901917e9ba1e9069717342e300c7058fbca2c15a
Balaji0001 <54093715+Balaji0001@users.noreply.github.com>
2019-08-20 16:50:37 +0530

    changed

--- src/org/tata/Employer.java
+++ src/org/tata/Employer.java
@@ -4,7 +4,6 @@ public class Employer {
 public static void main(String[] args) {
 	System.out.println("helloooo");
 	System.out.println("cool");
-	System.out.println("iuaefgiuqoe");
-	System.out.println("oqiehf");
+
 }
 }
