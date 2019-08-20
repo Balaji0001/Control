556ee0177b75a68f89f179d9e089b73a0faec018
Balaji0001 <balajivikram005@gmail.com>
2019-08-20 16:59:03 +0530

    sriygfiwuehguio

--- src/org/tata/Employer.java
+++ src/org/tata/Employer.java
@@ -2,8 +2,10 @@ package org.tata;
 
 public class Employer {
 public static void main(String[] args) {
-	System.out.println("helloooo");
-	System.out.println("cool");
+	int a,b;
+	a=10;
+	b=a;
+	System.out.println(b);
 
 }
 }
