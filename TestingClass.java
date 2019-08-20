a21e1d29fb83b66a4ab01a99b94136760a18b452
Balaji0001 <balajivikram005@gmail.com>
2019-08-20 10:45:04 +0530

    code added

---
+++ TestingClass.java
@@ -0,0 +1,16 @@
+package org.balaji;
+
+public class TestingClass {
+
+	public static void main(String[] args) {
+	 int a,b,c,i,j;
+	//To print even numbers
+	 for(i=0;i<=50;i++)
+	 {
+	  if (i%2==0) {
+		System.out.println(i);
+	}
+	 }
+	}
+	
+}
